f1e8ae63999fbe6ad69df17950a78af7d553100e
alxkinyanjui <alexgkinyanjui@gmail.com>
2023-07-13 20:37:06 +0000

    All Tasks

--- 0x02-minimum_operation/0-minoperations.py
+++ 0x02-minimum_operation/0-minoperations.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-
-""" Minimum Operations """
+"""
+Function that calculates the min operations to copy and paste letters
+"""
 
 
 def minOperations(n):
-    """
-    In a text file, there is a single character H. Your text editor can execute
-    only two operations in this file: Copy All and Paste. Given a number n,
-    write a method that calculates the fewest number of operations needed to
-    result in exactly n H characters in the file.
-    Returns an integer
-    If n is impossible to achieve, returns 0
-    """
-    if not isinstance(n, int):
-        return 0
-
-    op = 0
-    i = 2
-    while (i <= n):
-        if not (n % i):
-            n = int(n / i)
-            op += i
-            i = 1
-        i += 1
-    return op
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
