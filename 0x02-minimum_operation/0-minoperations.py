4b73ed7695c12bd68ece8ff39cd69903c7ca7037
alxkinyanjui <alexgkinyanjui@gmail.com>
2023-07-13 20:49:27 +0000

    All Tasks

--- 0x02-minimum_operation/0-minoperations.py
+++ 0x02-minimum_operation/0-minoperations.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""
-Function that calculates the min operations to copy and paste letters
-"""
+"""Module which contains minoperations function"""
 
 
 def minOperations(n):
-    nOpe = 0
-    minOpe = 2
-    while n > 1:
-        while n % minOpe == 0:
-            nOpe += minOpe
-            n /= minOpe
-        minOpe += 1
-    return nOpe
+    """ Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
+
+        Args:
+            n: repetitions of H
+
+        Returns:
+            number of operations (Copy & Paste) to reach n Hs
+    """
+    operations = 0
+    summation = 1
+    carrier = 0
+
+    while summation < n:
+        if n % summation == 0:  # Copy when summation is a multiple of n
+            carrier = summation
+            summation *= 2
+            operations += 1
+        else:
+            summation += carrier
+        operations += 1  # Always paste
+
+    return operations
